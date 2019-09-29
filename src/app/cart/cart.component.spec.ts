a5a1b1c3db21db238ec32ead126cdd204efad077
Chris Koch <christopherjkoch@comcast.net>
2019-09-29 19:51:50 -0400

    Added test for cart component

---
+++ src/app/cart/cart.component.spec.ts
@@ -0,0 +1,68 @@
+import { CartComponent } from "./cart.component";
+import { of } from "rxjs";
+
+describe('CartComponent', () => {
+    let component: CartComponent;
+    let CART;
+    let mockCartService;
+    let mockCartCalculatorService;
+    let mockRouter;
+
+    beforeEach(() => {
+        CART = [
+            {
+                "productId": 1,
+                "productName": "16lb bag of Skittles",
+                "productCategory": "Candy",
+                "price": 16.00,
+                "isImported": false
+            },
+            {
+                "productId": 2,
+                "productName": "Walkman",
+                "productCategory": "Electronics",
+                "price": 99.99,
+                "isImported": false
+            },
+            {
+                "productId": 3,
+                "productName": "bag of microwave Popcorn",
+                "productCategory": "Popcorn",
+                "price": 0.99,
+                "isImported": false
+            }
+        ]
+
+        mockCartService = jasmine.createSpyObj(['addToCart', 'getItems', 'clearCart']);
+        mockCartCalculatorService = jasmine.createSpyObj(['calculateTotal', 'calculateTotalTax', 'calculateLineItemTaxes']);
+        mockRouter = jasmine.createSpyObj(['navigate'])
+
+        component = new CartComponent(mockCartService, mockCartCalculatorService, mockRouter);
+    })
+
+    describe('emptyCart', () => {
+
+        it('should remove all items from the cart', () => {
+            mockCartService.clearCart.and.returnValue(true);
+            mockRouter.navigate.and.returnValue(true);
+            component.data = of(CART);
+
+            component.emptyCart();
+
+            let length = 0;
+            component.data.subscribe(r => { this.length = r.length });
+
+            expect(length).toBe(0);
+        })
+
+        it('should call clearCart', () => {
+            mockCartService.clearCart.and.returnValue(true);
+            mockRouter.navigate.and.returnValue(true);
+            component.data = of(CART);
+
+            component.emptyCart();
+
+            expect(mockCartService.clearCart).toHaveBeenCalledTimes(1);
+        })
+    })
+})
